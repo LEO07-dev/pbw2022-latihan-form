2730543816e13efc6daec70ecc3cf136f74ddfc2
Azmi Jalaluddin Amron <111202214400@mhs.dinus.ac.id>
2022-09-28 09:35:36 +0700

    Update data ke empat

---
+++ minggu4/Latihan php 0/proses1.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h2>Your Input:</h2>";
+echo $_POST['name'];
+echo "<br>";
+echo $_POST['email'];
+echo "<br>";
+echo $_POST['website'];
+echo "<br>";
+echo $_POST['comment'];
+echo "<br>";
+echo $_POST['gender'];
+?>
